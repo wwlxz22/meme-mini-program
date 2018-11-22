31eb7d98979657299c8d68df136e59c9f97a563b
wangwenchao <297782066@qq.com>
2018-11-22 16:43:11 +0800

    menu line componentlize

---
+++ components/line/line.js
@@ -0,0 +1,25 @@
+// components/line/line.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    lheight: Number,
+    lwidth: Number,
+    lcolor: String,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
