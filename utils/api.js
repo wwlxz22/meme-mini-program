ca288013eab227d7e88f0b6a2f89ab5dca18a38b
wangwenchao <297782066@qq.com>
2018-12-20 15:28:46 +0800

    login

--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,8 @@
-const DEVELOP_URL = `http://doutu.test/api`;
-const PRODUCT_URL = `https://doutu.xiaochongleyuan.com/api`;
+const DEVELOP_URL = `http://doutu.test/api/face`;
+const PRODUCT_URL = `https://doutu.xiaochongleyuan.com/api/face`;
 
-// const BASE_URL = DEVELOP_URL;
-const BASE_URL = PRODUCT_URL;
+const BASE_URL = DEVELOP_URL;
+// const BASE_URL = PRODUCT_URL;
 
 module.exports = {
   LOGIN: BASE_URL + `/user/login`, // 登陆接口
